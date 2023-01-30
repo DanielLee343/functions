fe942e566fe1d62e4247da025b30598127052429
Chameleon Cloud User <cc@nu-ins-1.novalocal>
2023-01-30 15:43:27 +0000

    add grant docker sock to cc

--- spawn_openfaas.sh
+++ spawn_openfaas.sh
@@ -6,3 +6,5 @@ sleep 2
 
 PASSWORD=$(kubectl get secret -n openfaas basic-auth -o jsonpath="{.data.basic-auth-password}" | base64 --decode; echo)
 echo -n $PASSWORD | faas-cli login --username admin --password-stdin
+
+sudo chmod 666 /var/run/docker.sock
