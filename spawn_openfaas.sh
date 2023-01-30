12678642cf0d9e7796291fc40d7af89b68ccc5d3
Chameleon Cloud User <cc@nu-ins-2.novalocal>
2023-01-30 14:55:49 +0000

    add spawn_openfaas

---
+++ spawn_openfaas.sh
@@ -0,0 +1,8 @@
+pkill -9 kubectl
+kubectl rollout status -n openfaas deploy/gateway
+kubectl port-forward -n openfaas svc/gateway 8080:8080 &
+
+sleep 2
+
+PASSWORD=$(kubectl get secret -n openfaas basic-auth -o jsonpath="{.data.basic-auth-password}" | base64 --decode; echo)
+echo -n $PASSWORD | faas-cli login --username admin --password-stdin
